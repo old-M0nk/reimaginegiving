6300cfe7e6f1421172d6f1461efaaaf2c4828e05
Arjun Mehra <arjun_mehra97@ymail.com>
2017-02-24 04:08:27 +0530

    changes in settings.py

--- reimagine/reimagine/settings.py
+++ reimagine/reimagine/settings.py
@@ -131,9 +131,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-]
+)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 print os.path.join(PROJECT_ROOT, 'static')
 
