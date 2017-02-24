cee4b71f366fadc7ad716b586da69bc7df8894a5
Arjun Mehra <arjun_mehra97@ymail.com>
2017-02-24 23:20:35 +0530

    push

--- reimagine/urls.py
+++ reimagine/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # Examples:
+    # url(r'^$', 'trust.views.home', name='home'),
+    # url(r'^trust/', include('trust.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('data.urls')), # for the main pages
+    url(r'^users/', include('users.urls')),# once the user logs in...
+    url(r'^staff/', include('staff.urls')), # for the staff portal
+
 ]
