4cdde5c82ce35fb02f2f607f1ff1098b4a2d16c5
Arjun Mehra <arjun_mehra97@ymail.com>
2017-02-27 13:08:50 +0530

    jugaad

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from .models import Email
+# from .models import Email
 
 
-class Email(forms.ModelForm):
-
-    class Meta:
-        model = Email
-        fields = ('email',)
+# class Email(forms.ModelForm):
+#
+#     class Meta:
+#         model = Email
+#         fields = ('email',)
