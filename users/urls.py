a2d5d18e8eb52f8a65516295a145bc1a21b7fa80
Arjun Mehra <arjun_mehra97@ymail.com>
2017-02-27 12:51:48 +0530

    jugaad

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from users import views
 
-urlpatterns = [
-        url(r'^$', views.comingSoon, name='comingSoon'),
-               ]
+# urlpatterns = [
+#         url(r'^$', views.comingSoon, name='comingSoon'),
+#                ]
