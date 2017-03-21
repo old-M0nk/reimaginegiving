35c6b63a35f752006bdd536c2095f04464627e76
Arjun Mehra <arjun_mehra97@ymail.com>
2017-03-21 23:48:59 +0530

    yo

--- data/models.py
+++ data/models.py
@@ -114,6 +114,7 @@ class GiveMonthly(models.Model):
 
 
 class TimelineEvent(models.Model):
+    project_id = models.ForeignKey('Project', on_delete=models.SET('project not set'))
     date = models.DateField(null=False)
     heading = models.TextField(blank=False)
     desc = models.TextField(blank=True)
