d28ca378ba597d788fe3dac19531bea341047bf4
Arjun Mehra <arjun_mehra97@ymail.com>
2017-03-21 22:47:06 +0530

    after rollback

--- data/models.py
+++ data/models.py
@@ -113,7 +113,7 @@ class GiveMonthly(models.Model):
         return self.project_id.title
 
 
-class TimelineEvents(models.Model):
+class TimelineEvent(models.Model):
     project_id = models.ForeignKey('Project', on_delete=models.SET('project not set'))
     date = models.DateField(null=False)
     heading = models.TextField(blank=False)
