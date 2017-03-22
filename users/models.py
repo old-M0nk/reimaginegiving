e1d4bcb18bb714766bec49565ecb7e0003a08a03
Arjun Mehra <arjun_mehra97@ymail.com>
2017-03-23 01:07:03 +0530

    yo

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from data.models import Project
 
 
 class Donor(models.Model):
-    donor_id = models.CharField(max_length=10, blank=False)
+    donor_id = models.ForeignKey(User, on_delete=models.SET('team member not set'))
     name = models.CharField(max_length=128, blank=False,)
     mobile = models.BigIntegerField(null = True)
     email = models.EmailField(max_length = 254)
@@ -18,7 +18,7 @@ class Donor(models.Model):
 
 class Donation(models.Model):
     transaction_id = models.BigIntegerField(null = False)
-    donor_id = models.ForeignKey('Donor', on_delete=models.SET('donor not assigned'))
+    donor_id = models.ForeignKey(User, on_delete=models.SET('team member not set'))
     project_id = models.ForeignKey(Project, on_delete=models.SET('project not set'))
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(null = False)
