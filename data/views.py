6226dde0f698e5fb624c8ec0292dcb76a33cb910
Arjun Mehra <arjun_mehra97@ymail.com>
2017-04-08 02:00:42 +0530

    'yo'

--- data/views.py
+++ data/views.py
@@ -190,7 +190,7 @@ def payment_redirect(request):
         purpose=request.POST['project'],
         send_email=True,
         email=request.POST['email'],
-        # redirect_url=
+        redirect_url=('comingSoon.html')
     )
     return render_to_response('comingSoon.html')
     # # print the long URL of the payment request.
