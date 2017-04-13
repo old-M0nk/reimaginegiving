ff004729ff63b281409cd00bf2647fa3667d13db
Arjun Mehra <arjun_mehra97@ymail.com>
2017-04-13 21:40:08 +0530

    try

--- data/views.py
+++ data/views.py
@@ -196,11 +196,11 @@ def payment_redirect(request):
     amount = request.POST["amount"]
     email = request.POST["email"]
     # phone = request.POST["mobile"]
-    project = request.POST["project"]
+    project = request.POST.get["project"]
     print project
     response = api.payment_request_create(
         amount=amount,
-        purpose=project,
+        purpose='yo',
         send_email=True,
         email=email
     )
